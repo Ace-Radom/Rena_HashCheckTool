e9957b1a8ccb9f10f0609bd903afb0bb8539f339
Ace_Radom <sichenradomlyu@gmail.com>
2023-03-26 11:38:12 +0200

    Console Extension: gotoxy() function support

---
+++ include/console_ext.h
@@ -0,0 +1,25 @@
+// console ext header
+
+#ifndef _CONSOLE_EXT_H_
+#define _CONSOLE_EXT_H_
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<Windows.h>
+
+#include"cps.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+__rhct_api int __cdecl wherex();
+__rhct_api int __cdecl wherey();
+__rhct_api void __cdecl gotoxy( int __x , int __y );
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ src/console_ext/consolepos.c
@@ -0,0 +1,19 @@
+#include"console_ext.h"
+
+__rhct_api int __cdecl wherex(){
+    CONSOLE_SCREEN_BUFFER_INFO pBuf;
+    GetConsoleScreenBufferInfo( GetStdHandle( STD_OUTPUT_HANDLE ) , &pBuf );
+    return pBuf.dwCursorPosition.X;
+}
+
+__rhct_api int __cdecl wherey(){
+    CONSOLE_SCREEN_BUFFER_INFO pBuf;
+    GetConsoleScreenBufferInfo( GetStdHandle( STD_OUTPUT_HANDLE ) , &pBuf );
+    return pBuf.dwCursorPosition.Y;
+}
+
+__rhct_api void __cdecl gotoxy( int __x , int __y ){
+    COORD pos = { __x , __y };
+    HANDLE hOut = GetStdHandle( STD_OUTPUT_HANDLE );
+    SetConsoleCursorPosition( hOut , pos );
+}
